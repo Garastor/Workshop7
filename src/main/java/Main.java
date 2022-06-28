933cdea1fad5b4222ad7868f3d68011b2f47ff6d
Garastor <tchornyi@griddynamics.com>
2022-06-28 13:32:03 +0300

    First Commit on master

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello Word");
+
+  }
+}
